1b2c8954d5258bf12537ba9306bcfce98944bec0
1hmed-ch <chakhmmani999@gmail.com>
2026-07-04 09:49:47 +0100

    core: initial setup of dockerized Spring Boot and React infrastructure

---
+++ FormaFlow/src/test/java/com/formaflow/formaflow/FormaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaflow.formaflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormaFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
